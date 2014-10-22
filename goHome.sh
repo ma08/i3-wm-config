3739d1842486288c94172580c2395618cb665239
mao8 <thisisma08@gmail.com>
2014-10-22 21:06:16 +0530

    Added goHome binding
    A binding which sends the current window to its designated
    workplace wherever the window is situated currently.
    
    Set for
    terminal, sytem monitor, firefox, pidgin and nautilus.

---
+++ goHome.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+a=$(xprop -id `xdotool getactivewindow` | grep WM_CLASS | cut -d '"' -f4)
+#notify-send -t 100 "$a"
+
+case $a in
+  Firefox )
+  i3-msg "move container to workspace 4" 
+    ;;
+  Gnome-terminal )
+  i3-msg "move container to workspace 3" 
+    ;;
+  Gnome-system-monitor )
+  i3-msg "move container to workspace 1" 
+    ;;
+  Nautilus )
+  i3-msg "move container to workspace 9" 
+    ;;
+  Pidgin )
+  i3-msg "move container to workspace 5" 
+    ;;
+esac
