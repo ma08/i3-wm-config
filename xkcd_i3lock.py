550d04b4a284d5a18dd34092442499c3f462fe90
mao8 <thisisma08@gmail.com>
2015-03-19 03:11:13 +0530

    Added choice saving to xkcd script

--- xkcd_i3lock.py
+++ xkcd_i3lock.py
@@ -5,6 +5,9 @@ p = Popen("ls ~/pro/xkcd/xkcd_comics_full_png/*.png", stdout=PIPE, shell=True)
 result=p.communicate()[0].split('\n')[0:-1]
 n=len(result)
 comic=result[randint(0,n)]
+f = open('/home/sourya/pro/xkcd/choices.txt', 'a')
+f.write(comic+"\n")
+f.close()
 call(["i3lock","-i",comic,"-t"])
 
 
